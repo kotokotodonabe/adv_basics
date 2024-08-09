1cf2a428185283513bddb9d04590bf79645b4b6c
kotokotodonabe <ksk.pleiades@gmail.com>
2024-08-10 00:02:19 +0900

    fix: 用意した質問データを画面に表示

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,6 +1,6 @@
 class QuizQuestion {
-  const QuizQuestion(this.text, this.questions);
+  const QuizQuestion(this.text, this.answers);
 
   final String text;
-  final List<String> questions;
+  final List<String> answers;
 }
